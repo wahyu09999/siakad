f12d5e5ee5faaa5c35d2b46c804684bdde1eb7ca
=wahyu09999 <=2041720191@student.polinema.ac.id>
2022-04-13 13:49:56 +0700

    Praktikum 9: ORM dengan relasi

---
+++ database/seeders/MataKuliahSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MataKuliahSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $matkul = [
+            [
+                'nama_matkul' => 'Pemprograman Berbasis Objek',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' =>4,
+            ],
+            [
+                'nama_matkul' => 'Pemprograman Web Lanjut',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' =>4,
+            ],
+            [
+                'nama_matkul' => 'Basis Data Lanjut',
+                'sks' => 3,
+                'jam' => 4,
+                'semester' =>6,
+            ],
+            [
+                'nama_matkul' => 'Praktikum Basis Data Lanjut',
+                'sks' => 3,
+                'jam' => 6,
+                'semester' =>4,
+            ],
+            
+        ];
+
+        DB::table('matakuliah')->insert($matkul);
+    }
+}
